a118899bafc680225731f057e0e17260cb249891
Srijan Samridh <srijansinha3@gmail.com>
2023-02-05 15:23:01 +0530

    api ready

--- API/api.py
+++ API/api.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/api', methods=['GET'])
+def returnascii():
+    d ={}
+    inputChar = str(request.args['query'])
+    answer = str(ord(inputChar))
+    d['output'] = answer
+    return d
+
+
+
+# def hello_world():
+#     return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run()
